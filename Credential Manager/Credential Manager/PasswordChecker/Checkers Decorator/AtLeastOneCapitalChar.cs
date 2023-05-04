8e92fee1695329d4d9d16fd18a86277079611af2
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-04 18:19:51 +0200

    create AtLeastOneCapitalChar checker decoratord

---
+++ Credential Manager/Credential Manager/PasswordChecker/Checkers Decorator/AtLeastOneCapitalChar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credential_Manager.PasswordChecker.Checkers_Decorator
+{
+    internal class AtLeastOneCapitalChar : PWChecker
+    {
+        public AtLeastOneCapitalChar(IPWChecker previousChecker, string password) : base(previousChecker, password)
+        {
+            FailMessage = "Password must contain at least one capital letter";
+        }
+
+        protected override bool CurrentCheck() => Password.Any(char.IsUpper);
+    }
+}
