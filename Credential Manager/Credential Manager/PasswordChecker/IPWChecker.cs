53f5211ccf02007f9e905af44569f308b1a269e4
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-04 17:33:45 +0200

    create parent Checker class

---
+++ Credential Manager/Credential Manager/PasswordChecker/IPWChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credential_Manager.PasswordChecker
+{
+    internal interface IPWChecker
+    {
+        public bool IsValid();
+        public string GetFailedRequirement();
+    }
+}
---
+++ Credential Manager/Credential Manager/PasswordChecker/PWChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credential_Manager.PasswordChecker
+{
+    internal abstract class PWChecker : IPWChecker
+    {
+        protected readonly IPWChecker _previousChecker;
+        protected readonly string _password;
+        protected string _failMessage;
+
+
+        public PWChecker(IPWChecker previousChecker, string password)
+        {
+            _previousChecker = previousChecker;
+            _password = password;
+        }
+        public bool IsValid() => _previousChecker.IsValid() && CurrentCheck();
+        public string GetFailedRequirement() =>
+            CurrentCheck() ?
+            $"{_previousChecker.GetFailedRequirement()}" :
+            $"{_previousChecker.GetFailedRequirement()}{_failMessage}";
+
+        protected abstract bool CurrentCheck();
+
+    }
+}
