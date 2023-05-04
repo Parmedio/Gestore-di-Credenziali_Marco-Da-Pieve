7ec1128608b9dba6942651a742cf935bc472cecc
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-04 17:51:58 +0200

    create AtLeastOneSpecialChar checker decoratord

---
+++ Credential Manager/Credential Manager/PasswordChecker/Checkers Decorator/AtLeastOneSpecialChar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credential_Manager.PasswordChecker.Checkers_Decorator
+{
+    internal class AtLeastOneSpecialChar : PWChecker
+    {
+        public AtLeastOneSpecialChar(IPWChecker previousChecker, string password) : base(previousChecker, password)
+        {
+            FailMessage = "Password must be at least one special char";
+        }
+
+
+        protected override bool CurrentCheck() => Password.Any(x => !char.IsLetterOrDigit(x));
+    }
+}
