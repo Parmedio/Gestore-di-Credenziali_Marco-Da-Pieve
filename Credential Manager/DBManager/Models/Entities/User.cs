73fc37a50ddfd8a47ef98bf12ecb9a8be65f6170
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-05 11:35:51 +0200

    create DBManager project

---
+++ Credential Manager/DBManager/Models/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBManager.Models.Entities;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? RegistrationDate { get; set; }
+}
---
+++ Credential Manager/DBManager/Repositories/DBContext/CredentialContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using DBManager.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBManager.Repositories.DBContext;
+
+public partial class CredentialContext : DbContext
+{
+    public CredentialContext()
+    {
+    }
+
+    public CredentialContext(DbContextOptions<CredentialContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=ACADEMYNETUD01\\SQLEXPRESS;Initial Catalog=CREDENTIALS; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__UsersID__PrimaryKey");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Password)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.RegistrationDate)
+                .HasMaxLength(8)
+                .IsUnicode(false);
+            entity.Property(e => e.UserName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
