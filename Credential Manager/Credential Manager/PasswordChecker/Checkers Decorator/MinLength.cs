2b366ffb2d6796059ff23ada1100ab8e01f636a6
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-05 10:09:10 +0200

    create PWChecksHandler

--- Credential Manager/Credential Manager/PasswordChecker/Checkers Decorator/MinLength.cs
+++ Credential Manager/Credential Manager/PasswordChecker/Checkers Decorator/MinLength.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Credential_Manager.PasswordChecker.Checkers_Decorator
+﻿namespace Credential_Manager.PasswordChecker.Checkers_Decorator
 {
     internal class MinLength : PWChecker
     {
---
+++ Credential Manager/Credential Manager/PasswordChecker/PWChecksHandler.cs
@@ -0,0 +1,22 @@
+﻿using Credential_Manager.PasswordChecker.Checkers_Decorator;
+
+namespace Credential_Manager.PasswordChecker
+{
+    internal static class PWChecksHandler
+    {
+        public static (bool, string) GetReport(string password)
+        {
+            IPWChecker PWChecker = Analyze(password);
+            return (PWChecker.IsValid(), PWChecker.GetFailedRequirement());
+        }
+        private static IPWChecker Analyze(string password)
+        {
+            IPWChecker passwordValidator = new PWBaseObject(password);
+            passwordValidator = new MinLength(passwordValidator, password);
+            passwordValidator = new AtLeastOneCapitalChar(passwordValidator, password);
+            passwordValidator = new AtLeastOneNumber(passwordValidator, password);
+            passwordValidator = new AtLeastOneSpecialChar(passwordValidator, password);
+            return passwordValidator;
+        }
+    }
+}
