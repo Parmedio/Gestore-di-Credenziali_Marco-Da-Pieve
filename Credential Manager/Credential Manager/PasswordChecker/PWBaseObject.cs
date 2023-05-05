5f501c83a37bfa3d7d98910cb275203611003dcd
Marco Da Pieve <marco.da.pieve@gmail.com>
2023-05-05 10:07:29 +0200

    create PWBaseObject

---
+++ Credential Manager/Credential Manager/PasswordChecker/PWBaseObject.cs
@@ -0,0 +1,12 @@
+﻿namespace Credential_Manager.PasswordChecker
+{
+    internal class PWBaseObject : IPWChecker
+    {
+        private string _password;
+
+        public PWBaseObject(string password) => _password = password;
+
+        public bool IsValid() => _password != string.Empty;
+        public string GetFailedRequirement() => IsValid() ? string.Empty : "- Password is empty.\n";
+    }
+}
